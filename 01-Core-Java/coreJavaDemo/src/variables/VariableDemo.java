18c3f086be55a49c497c7a75b485b16e077a599a
bhaveshkundwani18 <bhavesh@alphaspec.in>
2026-07-06 15:24:49 +0530

    Add Java variables examples

---
+++ 01-Core-Java/coreJavaDemo/src/variables/VariableDemo.java
@@ -0,0 +1,29 @@
+package variables;
+
+public class VariableDemo {
+	public static void main(String[] args) {
+		// Declaring and initializing variables
+        
+        // Integer variable  
+        int age = 25;       
+        
+        // String variable  
+        String name = "Bhavesh"; 
+        
+        // Double variable 
+        double salary = 50000.50;    
+        
+        // Float variable
+        float si = 5.5f;
+        
+        // Character variable
+        char var = 'h';  
+
+        // Displaying the values of variables
+        System.out.println("Age: " + age);          
+        System.out.println("Name: " + name);        
+        System.out.println("Salary: " + salary);
+        System.out.println("Simple Interest: " + si);  
+        System.out.println("Character: " + var);
+	}
+}
