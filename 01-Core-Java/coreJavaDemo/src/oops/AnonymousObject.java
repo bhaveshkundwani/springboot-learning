ee13ecc3b4c13a4dd125dc857a3da0d37a140826
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-07-09 11:40:34 +0530

    Add Java Static Keyword, Naming Convention and Anonymous Object in OOPS concepts

---
+++ 01-Core-Java/coreJavaDemo/src/oops/AnonymousObject.java
@@ -0,0 +1,33 @@
+package oops;
+
+//class A {
+//	
+//	public A() {
+//		System.out.println("object created");
+//	}
+//	
+//	public void show() {
+//		System.out.println("in A show");
+//	}
+//	
+//}
+
+public class AnonymousObject {
+
+	public static void main(String[] args) {
+		
+//		int marks;
+//		marks = 99;
+//		
+//		new A();   // anonymous object because this type of object do not have a name
+//		new A().show();
+//		
+////		A obj = new A();
+//		A obj;
+//		obj = new A();
+//		
+//		obj.show();
+		
+	}
+
+}
