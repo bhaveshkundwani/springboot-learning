c88ca777cbf78c87c0dfe71519a804906ea1d088
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-07-10 00:47:06 +0530

    Add More Java Concepts

---
+++ 01-Core-Java/coreJavaDemo/src/oops/ObjectClass.java
@@ -0,0 +1,56 @@
+package oops;
+
+import java.util.Objects;
+
+//class Laptop {
+//	
+//	String model;
+//	int price;
+////	String serial;
+//	
+//	@Override
+//	public int hashCode() {
+//		return Objects.hash(model, price);
+//	}
+//	
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Laptop other = (Laptop) obj;
+//		return Objects.equals(model, other.model) && price == other.price;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Laptop [model=" + model + ", price=" + price + "]";
+//	}
+//	
+//}
+
+public class ObjectClass {
+
+	public static void main(String[] args) {
+		
+//		Laptop obj = new Laptop();
+//    	obj.model = "Lenevo Yoga";
+//    	obj.price = 1000;
+//    	
+//    	Laptop obj2 = new Laptop();
+//    	obj2.model = "Lenevo Yoga";
+// //   	obj2.model = "Lenevo Yoga1";
+//    	obj2.price = 1000;
+//    	
+//    	boolean result = obj.equals(obj2);
+//    	
+//    	System.out.println(result);
+//    	System.out.println(obj.toString());
+////   	System.out.println(obj);
+    	
+	}
+
+}
