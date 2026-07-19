3df572173adbcc25b053d2228ef4b3b085eac7a2
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-07-19 15:33:44 +0530

    Add xml config file

---
+++ 04-Spring-Framework/02-Spring-Bean-XML-Config/src/main/java/com/spring/App.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+        
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");  // create a container
+    	
+//    	Alien obj = new Alien();
+    	Alien obj = (Alien) context.getBean("alien");
+    	obj.code();
+    	
+    }
+}
