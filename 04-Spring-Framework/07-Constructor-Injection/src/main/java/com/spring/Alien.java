db958c8656df9605266b1789fd1010209f9dfa87
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-07-22 20:13:15 +0530

    Learn constructor DI in spring

---
+++ 04-Spring-Framework/07-Constructor-Injection/src/main/java/com/spring/Alien.java
@@ -0,0 +1,44 @@
+package com.spring;
+
+import java.beans.ConstructorProperties;
+
+public class Alien {
+	
+	private int age;
+	
+	private Laptop lap;
+
+	public Alien() {
+		System.out.println("Object Created");
+	}
+	
+	@ConstructorProperties({"age", "lap"})
+	public Alien(int age, Laptop lap) {
+		System.out.println("Para Constructor Called");
+		this.age = age;
+		this.lap = lap;
+	}
+
+	public void code() {
+		System.out.println("Coding");
+		lap.compile();
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		System.out.println("Setter Called");
+		this.age = age;
+	}
+
+	public Laptop getLap() {
+		return lap;
+	}
+
+	public void setLap(Laptop lap) {
+		this.lap = lap;
+	}
+	
+}
---
+++ 04-Spring-Framework/07-Constructor-Injection/src/main/java/com/spring/Laptop.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+public class Laptop {
+
+	public Laptop() {
+		System.out.println("Laptop object created");
+	}
+	
+	public void compile() {
+		System.out.println("Compiling");
+	}
+	
+}
