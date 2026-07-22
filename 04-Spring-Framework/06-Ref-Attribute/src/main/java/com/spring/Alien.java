c88fb6fed33d8da851834d74dcb084212ec7ae9b
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-07-22 11:43:31 +0530

    Learn reference attribute in spring xml

---
+++ 04-Spring-Framework/06-Ref-Attribute/src/main/java/com/spring/Alien.java
@@ -0,0 +1,35 @@
+package com.spring;
+
+public class Alien {
+	
+	private int age;
+	
+	private Laptop lap;
+
+	public Alien() {
+		System.out.println("Object Created");
+	}
+	
+	public void code() {
+		System.out.println("Coding");
+		lap.compile();
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		System.out.println("Setter Called");
+		this.age = age;
+	}
+
+	public Laptop getLap() {
+		return lap;
+	}
+
+	public void setLap(Laptop lap) {
+		this.lap = lap;
+	}
+	
+}
