b26a466e309911c03ffe829155a20922674c0455
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-07-25 19:40:26 +0530

    Learn more about beans

---
+++ 04-Spring-Framework/13-Inner-Beans/src/main/java/com/spring/App.java
@@ -0,0 +1,30 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+        
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");  // create a container
+    	
+//    	Alien obj1 = (Alien) context.getBean("alien1");
+    	
+    	Alien obj1 = context.getBean("alien1", Alien.class);
+//    	obj1.setAge(21);
+    	System.out.println(obj1.getAge());
+    	obj1.code();
+    	
+//    	Alien obj2 = (Alien) context.getBean("alien1");
+//    	System.out.println(obj2.age);
+    	//obj2.code();
+ 
+    	
+//    	Desktop obj = (Desktop) context.getBean("com2");
+    	
+//    	Desktop obj = context.getBean("com2", Desktop.class);
+    	
+    	//Desktop obj = context.getBean(Desktop.class);
+    	
+    }
+}
