77b8e20ee6beda14c92bc670ab5e5ef44c231485
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-08-22 08:09:16 +0530

    Add Spring MVC controller example with Thymeleaf

---
+++ 08-Spring-Boot-Web/Thymeleaf/03-Creating-Controller/src/main/java/com/bhavesh/springboot/HomeController.java
@@ -0,0 +1,13 @@
+package com.bhavesh.springboot;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+	public String home() {
+		System.out.println("Home method called");
+		return "index";
+	}
+	
+}
