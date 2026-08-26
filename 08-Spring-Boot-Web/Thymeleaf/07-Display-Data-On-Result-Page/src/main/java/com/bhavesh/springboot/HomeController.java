09c26c63b80aa7c926b3496222d17b8cf38feed3
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-08-26 11:26:11 +0530

    Add data display on Thymeleaf result page

---
+++ 08-Spring-Boot-Web/Thymeleaf/07-Display-Data-On-Result-Page/src/main/java/com/bhavesh/springboot/HomeController.java
@@ -0,0 +1,27 @@
+package com.bhavesh.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("Home method called");
+		return "index";
+	}
+	
+	@RequestMapping("add")
+	public String add(HttpServletRequest req, HttpSession session) {
+		
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+        session.setAttribute("result", result);
+		return "result";
+	}
+}
