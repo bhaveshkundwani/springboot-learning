8bb8632fc7317b9c4fd4ee7b212768281f9775ec
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-09-13 12:58:19 +0530

    Add @PutMapping and @DeleteMapping example

--- 09-Spring-Boot-REST/spring-boot-rest/src/main/java/com/jobapp/controller/JobRestController.java
+++ 09-Spring-Boot-REST/spring-boot-rest/src/main/java/com/jobapp/controller/JobRestController.java
@@ -3,9 +3,11 @@ package com.jobapp.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +38,18 @@ public class JobRestController {
     public JobPost addJob(@RequestBody JobPost jobPost) {
         service.addJob(jobPost);
         return service.getJob(jobPost.getPostId());
+    }
+
+	@PutMapping("jobPost")
+    public JobPost updateJob(@RequestBody JobPost jobPost) {
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
+
+    @DeleteMapping("jobPost/{postId}")
+    public String deleteJob(@PathVariable int postId) {
+        service.deleteJob(postId);
+        return "Deleted";
     }
 
 }
--- 09-Spring-Boot-REST/spring-boot-rest/src/main/java/com/jobapp/repo/JobRepo.java
+++ 09-Spring-Boot-REST/spring-boot-rest/src/main/java/com/jobapp/repo/JobRepo.java
@@ -45,12 +45,34 @@ public class JobRepo {
     
 	public JobPost getJob(int postId) {
 		
-	  for(JobPost job : jobs) {
-	      if (job.getPostId() == postId)
-	          return job;
-	  }
+		for(JobPost job : jobs) {
+			if (job.getPostId() == postId)
+				return job;
+		}
 	  
-	  return null;
+		return null;
+	}
+
+	public void updateJob(JobPost jobPost) {
+		
+		for (JobPost jobPost1 : jobs) {
+	        if (jobPost1.getPostId() == jobPost.getPostId()) {
+	            jobPost1.setPostProfile(jobPost.getPostProfile());
+	            jobPost1.setPostDesc(jobPost.getPostDesc());
+	            jobPost1.setReqExperience(jobPost.getReqExperience());
+	            jobPost1.setPostTechStack(jobPost.getPostTechStack());
+	        }
+	    }
+		
+	}
+	
+	public void deleteJob(int postId) {
+		
+		for (JobPost jobPost : jobs) {
+			if (jobPost.getPostId() == postId)
+				jobs.remove(jobPost);
+		}
+		
 	}
     
 }
