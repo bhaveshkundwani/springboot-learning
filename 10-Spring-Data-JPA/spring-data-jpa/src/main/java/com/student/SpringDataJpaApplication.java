e4cbc0a7d5c1014f31ba206eb37ce92064656a6d
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-09-13 16:17:46 +0530

    Setup, create table and inserting data using JPA

---
+++ 10-Spring-Data-JPA/spring-data-jpa/src/main/java/com/student/SpringDataJpaApplication.java
@@ -0,0 +1,43 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.student.model.Student;
+import com.student.repository.StudentRepository;
+
+@SpringBootApplication
+public class SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = SpringApplication.run(SpringDataJpaApplication.class, args); 
+		
+		StudentRepository repo = context.getBean(StudentRepository.class);
+		
+		Student s1 = context.getBean(Student.class);
+		Student s2 = context.getBean(Student.class);
+		Student s3 = context.getBean(Student.class);
+
+		s1.setRollNo(101);
+		s1.setName("Navin");
+		s1.setMarks(75);
+
+		s2.setRollNo(102);
+		s2.setName("Kiran");
+		s2.setMarks(80);
+
+		s3.setRollNo(103);
+		s3.setName("Bhavesh");
+		s3.setMarks(81);
+		
+		repo.save(s1);
+		repo.save(s2);
+		repo.save(s3);
+		
+		
+		
+	}
+
+}
