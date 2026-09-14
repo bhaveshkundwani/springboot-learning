070fdeeb152241774f1478ce1a556e7a7c0500aa
bhaveshkundwani <kundwanibhavesh@gmail.com>
2026-09-14 16:27:05 +0530

    Learn and implement findAll() and findById() methods of JPA

--- 10-Spring-Data-JPA/spring-data-jpa/src/main/java/com/student/SpringDataJpaApplication.java
+++ 10-Spring-Data-JPA/spring-data-jpa/src/main/java/com/student/SpringDataJpaApplication.java
@@ -1,5 +1,7 @@
 package com.student;
 
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -20,24 +22,28 @@ public class SpringDataJpaApplication {
 		Student s2 = context.getBean(Student.class);
 		Student s3 = context.getBean(Student.class);
 
-		s1.setRollNo(101);
-		s1.setName("Navin");
-		s1.setMarks(75);
-
-		s2.setRollNo(102);
-		s2.setName("Kiran");
-		s2.setMarks(80);
-
-		s3.setRollNo(103);
-		s3.setName("Bhavesh");
-		s3.setMarks(81);
+//		s1.setRollNo(101);
+//		s1.setName("Navin");
+//		s1.setMarks(75);
+//
+//		s2.setRollNo(102);
+//		s2.setName("Kiran");
+//		s2.setMarks(80);
+//
+//		s3.setRollNo(103);
+//		s3.setName("Bhavesh");
+//		s3.setMarks(81);
+//		
+//		repo.save(s1);
+//		repo.save(s2);
+//		repo.save(s3);
 		
-		repo.save(s1);
-		repo.save(s2);
-		repo.save(s3);
+		System.out.println(repo.findAll());
 		
-		
-		
+		Optional<Student> s = repo.findById(105);
+		System.out.println(s.orElse(new Student()));
+		System.out.println(repo.findById(101));
+		 
 	}
 
 }
